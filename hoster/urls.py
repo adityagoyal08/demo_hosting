687c9247ef45ad551bdb38298c54ee55aa7040f0
Aditya <88126377+eddyhacks@users.noreply.github.com>
2024-07-04 23:11:23 +0530

    Update urls.py
    paths are changed

--- hoster/urls.py
+++ hoster/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("host/", include("host.urls")),
-    path('', admin.site.urls),
+    path("", include("host.urls")),
+    path("admin/", admin.site.urls),
 ]
